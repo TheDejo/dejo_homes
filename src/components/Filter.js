c9135bc274ba181a4e96a7d2b5e959a69d7c8d3f
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-08-18 21:16:40 -0500

    added two new components

---
+++ src/components/Filter.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+function Filter() {
+  return (
+    <section id="filter">
+      <div className="inside">
+          <h1>Filters</h1>
+        <select name="neighborhood" className="filter-neighborhood">
+          <option>Oakville</option>
+        </select>
+        <select name="housetype" className="filter-housetype">
+          <option>Single Story</option>
+        </select>
+        <select name="rooms" className=" filter-rooms">
+          <option>2 br</option>
+        </select>
+        <select name="baths" className="filter-baths">
+          <option>1 ba</option>
+        </select>
+        <div className="filter-price">
+          <span className="title">Price</span>
+            <input type="text" name="min-price" className="min-price" placeholder="min price"/>
+            <input type="text" name="max-price" className="max-price" placeholder="max price"/>
+        </div>
+        <div className="filter-square-feet">
+          <span className="title">Square Feet</span>
+            <input type="text" name="min-square-feet" className="min-square-feet" placeholder="min sqft"/>
+            <input type="text" name="max-square-feet" className="max-square-feet" placeholder="max sqft"/>
+        </div>
+        <div id="bottom-filters" className="filter-extras">
+          <span className="title">
+            Extras
+          </span>
+          <label htmlFor="extras">
+            <span>Pool</span>
+            <input name="extras" value="pool" type="checkbox"/>
+          </label>
+          <label htmlFor="extras">
+            <span>Finished Basement</span>
+            <input name="extras" value="finished-basement" type="checkbox"/>
+          </label>
+          <label htmlFor="extras">
+            <span>Two Car Garage</span>
+            <input name="extras" value="two-car-garage" type="checkbox"/>
+          </label>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Filter;
---
+++ src/components/Listings.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+function Listings() {
+  return (
+      <section id="listings">
+        <section className="search-area">
+          <input type='text' name="search"/>
+        </section>
+
+        <section className="sort">
+          <div className='main-results'>
+            390 results found
+          </div>
+          <div className="sort-options">
+            <select name="sort-by" className="sort-by">
+              <option value="price-ascending">Highest Price</option>
+              <option value="price-ascending">Lowest Price</option>
+            </select>
+            <div className="view">
+              <i className="fa fa-list-ul"></i>
+              <i className="fa fa-th"></i>
+            </div>
+          </div>
+        </section>
+
+        <section className="list-results">
+          <div className="list">
+            <div className="list-img">
+              <span className="address">Address</span>
+              <div className="details">
+                <div className="user-img"></div>
+                <div className="user-details">
+                  <span className="user-name">Marcus Aurelius</span>
+                  <span className="post-date">08/10/2019</span>
+                </div>
+                <div className="list-details">
+                  <div className="floor-space">
+                    <i className="fa fa-home"></i>
+                      <span>2500 ft&sup2;</span>
+                  </div>
+                  <div className="bedrooms">
+                    <i className="fa fa-bed"></i>
+                      <span>3 br</span>
+                  </div>
+                  <div className="baths">
+                    <i className="fa fa-bath"></i>
+                      <span>2 ba</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bottom-info">
+              <span>$1450 / mo</span>
+              <span><i className="fa fa-map-marker"></i>Oakville,MO</span>
+            </div>
+          </div>
+        </section>
+
+        <section className="pagination">
+          <ul className="pagination-numbers">
+            <li>Prev</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>Next</li>
+          </ul>
+        </section>
+
+      </section>
+  );
+}
+
+export default Listings;
