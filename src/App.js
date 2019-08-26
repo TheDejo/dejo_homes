d93ee4f7da10991b730fccef037a7684f898ded2
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-08-25 21:48:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
+import Filter from './components/Filter';
+import Listings from './components/Listings';
 import './App.css';
+import './Header.css';
+import './Filter.css';
+import './Listings.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <section id='content-area'>
+        <Filter/>
+        <Listings/>
+      </section>
     </div>
   );
 }
-
 export default App;
