8a5c4e21ad59f0258dd4ac6637bbdb97dd848ae1
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-08-25 22:59:09 -0500

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header.js';
+import Filter from './Filter.js';
+import Listings from './Listings.js';
+import '../App.css';
+import '../Header.css';
+import '../Filter.css';
+import '../Listings.css';
+import listingsData from "./data/listingsData.js";
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Adam',
+      listingsData
+    }
+  }
+// function App() {
+render () {
+  console.log(this.state.listingsData);
+  return (
+    <div>
+      <Header/>
+      <section id='content-area'>
+        <Filter/>
+        <Listings/>
+      </section>
+    </div>
+  );
+}
+}
+export default App;
+const root = document.getElementById('root');
+ReactDOM.render(<App />,root);
---
+++ src/components/data/listingsData.js
@@ -0,0 +1,74 @@
+var listingsData = [
+  {
+    address: "25 Parkway Dr.",
+    city: 'Clayton',
+    state: 'Missori',
+    rooms: 4,
+    price: 565000,
+    floorSpace: 3200,
+    extras:[
+      'finished basement',
+      'two car garage'
+    ],
+    homeType:'two story',
+    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+  },
+  {
+    address: "25 Parkway Dr.",
+    city: 'Clayton',
+    state: 'Missori',
+    rooms: 4,
+    price: 565000,
+    floorSpace: 3200,
+    extras:[
+      'finished basement',
+      'two car garage'
+    ],
+    homeType:'two story',
+    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+  },
+  {
+    address: "25 Parkway Dr.",
+    city: 'Clayton',
+    state: 'Missori',
+    rooms: 4,
+    price: 565000,
+    floorSpace: 3200,
+    extras:[
+      'finished basement',
+      'two car garage'
+    ],
+    homeType:'two story',
+    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+  },
+  {
+    address: "25 Parkway Dr.",
+    city: 'Clayton',
+    state: 'Missori',
+    rooms: 4,
+    price: 565000,
+    floorSpace: 3200,
+    extras:[
+      'finished basement',
+      'two car garage'
+    ],
+    homeType:'two story',
+    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+  },
+  {
+    address: "25 Parkway Dr.",
+    city: 'Clayton',
+    state: 'Missori',
+    rooms: 4,
+    price: 565000,
+    floorSpace: 3200,
+    extras:[
+      'finished basement',
+      'two car garage'
+    ],
+    homeType:'two story',
+    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+  }
+]
+
+export default listingsData;
