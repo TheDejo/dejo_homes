c55733781be86dcde896812efd1aacb092f98eea
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-08-26 21:22:43 -0500

    added object

--- src/components/data/listingsData.js
+++ src/components/data/listingsData.js
@@ -2,8 +2,9 @@ var listingsData = [
   {
     address: "25 Parkway Dr.",
     city: 'Clayton',
-    state: 'Missori',
+    state: 'MO',
     rooms: 4,
+    bath: 2,
     price: 565000,
     floorSpace: 3200,
     extras:[
@@ -14,60 +15,64 @@ var listingsData = [
     image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
   },
   {
-    address: "25 Parkway Dr.",
-    city: 'Clayton',
-    state: 'Missori',
-    rooms: 4,
-    price: 565000,
-    floorSpace: 3200,
+    address: "983 Maxwell Pl",
+    city: 'Kirkwood',
+    state: 'MO',
+    rooms: 3,
+    bath: 2,
+    price: 375000,
+    floorSpace: 2600,
     extras:[
       'finished basement',
-      'two car garage'
+      'two car garage',
+      'pool'
     ],
-    homeType:'two story',
-    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+    homeType:'single story',
+    image: 'https://i.pinimg.com/originals/dc/89/bf/dc89bf25763e07fac8477534b3fe300c.jpg'
   },
   {
-    address: "25 Parkway Dr.",
-    city: 'Clayton',
-    state: 'Missori',
-    rooms: 4,
-    price: 565000,
-    floorSpace: 3200,
+    address: "10 Huntsport Rd.",
+    city: 'Oakville',
+    state: 'MO',
+    rooms: 2,
+    bath: 1,
+    price: 175000,
+    floorSpace: 2100,
     extras:[
       'finished basement',
-      'two car garage'
     ],
-    homeType:'two story',
-    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+    homeType:'single story',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTngfDVgOMO5vDTejvzVTWdTDsP4khst8-TM4FEDPm0aMHQI5Qvsg'
   },
   {
-    address: "25 Parkway Dr.",
-    city: 'Clayton',
-    state: 'Missori',
+    address: "3690 Principle Ln.",
+    city: 'Chesterfield',
+    state: 'MO',
     rooms: 4,
-    price: 565000,
-    floorSpace: 3200,
+    bath: 2.5,
+    price: 750000,
+    floorSpace: 3900,
     extras:[
       'finished basement',
-      'two car garage'
+      'two car garage',
+      'pool'
     ],
     homeType:'two story',
-    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdBggUKxFndfmWgFDdDJmO1EMi8f8PgpbGCQ1hNQ-QyHCT7Oktdw'
   },
   {
-    address: "25 Parkway Dr.",
-    city: 'Clayton',
-    state: 'Missori',
-    rooms: 4,
-    price: 565000,
-    floorSpace: 3200,
+    address: "321 Osiris St.",
+    city: 'Ladue',
+    state: 'MO',
+    rooms: 5,
+    bath: 3,
+    price: 435000,
+    floorSpace: 3350,
     extras:[
-      'finished basement',
       'two car garage'
     ],
     homeType:'two story',
-    image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAUVN1dNUXLpwybly83GuUpc-6l_NHRLbGgWecoJXfHycA40aalQ'
   }
 ]
 
