2743d9ce4eba5a1716db25d7bbaa241ef370dc07
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-08-29 19:13:43 -0500

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -14,27 +14,35 @@ render () {
     <section id="filter">
       <div className="inside">
           <h1>Filters</h1>
-        <select name="neighborhood" className="filter-neighborhood">
-          <option>Oakville</option>
+        <select name="neighborhood" className="filter-neighborhood" onChange={this.props.change}>
+          <option value='Oakville'>Oakville</option>
+          <option value='Kirkwood'>Kirkwood</option>
         </select>
-        <select name="housetype" className="filter-housetype">
-          <option>Single Story</option>
+        <select name="housetype" className="filter-housetype" onChange={this.props.change}>
+          <option value='Single Story'>Single Story</option>
+          <option value='Two Story'>Two Story</option>
         </select>
-        <select name="rooms" className=" filter-rooms">
-          <option>2 br</option>
+        <select name="rooms" className=" filter-rooms" onChange={this.props.change}>
+          <option value='1'>1 br</option>
+          <option value='2'>2 br</option>
+          <option value='3'>3 br</option>
+          <option value='4'>4 br</option>
         </select>
-        <select name="baths" className="filter-baths">
-          <option>1 ba</option>
+        <select name="baths" className="filter-baths" onChange={this.props.change}>
+          <option value='1'>1 ba</option>
+          <option value='2'>2 ba</option>
+          <option value='3'>3 ba</option>
+          <option value='4'>4 ba</option>
         </select>
         <div className="filter-price">
           <span className="title">Price</span>
-            <input type="text" name="min-price" className="min-price" placeholder="min price"/>
-            <input type="text" name="max-price" className="max-price" placeholder="max price"/>
+            <input type="text" name="min_price" className="min-price" placeholder="min price" value={this.props.globalState.min_price} onChange={this.props.change}/>
+            <input type="text" name="max_price" className="max-price" placeholder="max price" value={this.props.globalState.max_price} onChange={this.props.change}/>
         </div>
         <div className="filter-square-feet">
           <span className="title">Square Feet</span>
-            <input type="text" name="min-square-feet" className="min-square-feet" placeholder="min sqft"/>
-            <input type="text" name="max-square-feet" className="max-square-feet" placeholder="max sqft"/>
+            <input type="text" name="min_square_feet" className="min-square-feet" placeholder="min sqft" value={this.props.globalState.min_square_feet} onChange={this.props.change}/>
+            <input type="text" name="max_square_feet" className="max-square-feet" placeholder="max sqft" value={this.props.globalState.max_square_feet} onChange={this.props.change}/>
         </div>
         <div id="bottom-filters" className="filter-extras">
           <span className="title">
@@ -42,15 +50,15 @@ render () {
           </span>
           <label htmlFor="extras">
             <span>Pool</span>
-            <input name="extras" value="pool" type="checkbox"/>
+            <input name="pool" value="pool" type="checkbox" onChange={this.props.change}/>
           </label>
           <label htmlFor="extras">
             <span>Finished Basement</span>
-            <input name="extras" value="finished-basement" type="checkbox"/>
+            <input name="finished_basement" value="finished_basement" type="checkbox" onChange={this.props.change}/>
           </label>
           <label htmlFor="extras">
             <span>Two Car Garage</span>
-            <input name="extras" value="two-car-garage" type="checkbox"/>
+            <input name="two_car_garage" value="two_car_garage" type="checkbox" onChange={this.props.change}/>
           </label>
         </div>
       </div>
@@ -59,4 +67,3 @@ render () {
 }
 }
 export default Filter;
-
