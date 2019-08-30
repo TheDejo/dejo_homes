0906fa33558c078d5c929979d051e34f9e5c775c
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-08-30 17:21:52 -0500

    Update listingsData.js

--- src/components/data/listingsData.js
+++ src/components/data/listingsData.js
@@ -1,7 +1,7 @@
 var listingsData = [
   {
     address: "25 Parkway Dr.",
-    city: 'Clayton',
+    neighborhood: 'Clayton',
     state: 'MO',
     rooms: 4,
     bath: 2,
@@ -11,12 +11,12 @@ var listingsData = [
       'finished basement',
       'two car garage'
     ],
-    homeType:'two story',
+    houseType:'two story',
     image: 'https://www.houseopedia.com/wp-content/uploads/2017/03/AdobeStock_112041533-e1489428614840.jpeg'
   },
   {
     address: "983 Maxwell Pl",
-    city: 'Kirkwood',
+    neighborhood: 'Kirkwood',
     state: 'MO',
     rooms: 3,
     bath: 2,
@@ -27,12 +27,12 @@ var listingsData = [
       'two car garage',
       'pool'
     ],
-    homeType:'single story',
+    houseType:'single story',
     image: 'https://i.pinimg.com/originals/dc/89/bf/dc89bf25763e07fac8477534b3fe300c.jpg'
   },
   {
     address: "10 Huntsport Rd.",
-    city: 'Oakville',
+    neighborhood: 'Oakville',
     state: 'MO',
     rooms: 2,
     bath: 1,
@@ -41,12 +41,12 @@ var listingsData = [
     extras:[
       'finished basement',
     ],
-    homeType:'single story',
+    houseType:'single story',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTngfDVgOMO5vDTejvzVTWdTDsP4khst8-TM4FEDPm0aMHQI5Qvsg'
   },
   {
     address: "3690 Principle Ln.",
-    city: 'Chesterfield',
+    neighborhood: 'Chesterfield',
     state: 'MO',
     rooms: 4,
     bath: 2.5,
@@ -57,12 +57,12 @@ var listingsData = [
       'two car garage',
       'pool'
     ],
-    homeType:'two story',
+    houseType:'two story',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdBggUKxFndfmWgFDdDJmO1EMi8f8PgpbGCQ1hNQ-QyHCT7Oktdw'
   },
   {
     address: "321 Osiris St.",
-    city: 'Ladue',
+    neighborhood: 'Ladue',
     state: 'MO',
     rooms: 5,
     bath: 3,
@@ -71,7 +71,7 @@ var listingsData = [
     extras:[
       'two car garage'
     ],
-    homeType:'two story',
+    houseType:'two story',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAUVN1dNUXLpwybly83GuUpc-6l_NHRLbGgWecoJXfHycA40aalQ'
   }
 ]
