501d4ce7b333188c3672eb138939e001a7df8a2a
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-09-10 23:00:42 -0500

    added dropdown filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,11 +6,29 @@ class Filter extends Component {
     super()
     this.state = {
       name: 'Adam',
+      showFilter: false, // new
     }
     this.neighborhoods = this.neighborhoods.bind(this)
     this.houseTypes = this.houseTypes.bind(this)
     this.beds = this.beds.bind(this)
     this.bath = this.bath.bind(this)
+    this.showFilter = this.showFilter.bind(this) //new
+    this.closeFilter = this.closeFilter.bind(this) //new
+  }
+
+  // new
+  showFilter(event) {
+    event.preventDefault();
+    this.setState({showFilter: true}, () => {
+      document.addEventListener('click',this.closeFilter)
+    });
+  }
+  closeFilter(event) {
+    if (!this.dropMenu.contains(event.target)) {
+    this.setState({showFilter: false}, () => {
+      document.removeEventListener('click',this.closeFilter)
+      });
+    }
   }
   componentWillMount() {
     this.props.populateAction()
@@ -63,54 +81,62 @@ class Filter extends Component {
 render () {
   return (
     <section id="filter">
-      <div className="inside">
-          <h1>Filter</h1>
-        <span className='title'>City</span>
-        <select name="neighborhood" className="filter-neighborhood" onChange={this.props.change}>
-          <option value='All'>All</option>
-            {this.neighborhoods()}
-        </select>
-        <span className='title'>Type</span>
-        <select name="houseType" className="filter-houseType" onChange={this.props.change}>
-          <option value='All'>All</option>
-          {this.houseTypes()}
-        </select>
-        <span className='title'>Bedrooms</span>
-        <select name="bedrooms" className=" filter-rooms" onChange={this.props.change}>
-          {this.beds()}
-        </select>
-        <span className='title'>Baths</span>
-        <select name="baths" className="filter-baths" onChange={this.props.change}>
-          {this.bath()}
-        </select>
-        <div className="filter-price">
-          <span className="title">Price</span>
-            <input type="text" name="min_price" className="min-price" placeholder="min price" value={this.props.globalState.min_price} onChange={this.props.change}/>
-            <input type="text" name="max_price" className="max-price" placeholder="max price" value={this.props.globalState.max_price} onChange={this.props.change}/>
-        </div>
-        <div className="filter-square-feet">
-          <span className="title">Square Feet</span>
-            <input type="text" name="min_square_feet" className="min-square-feet" placeholder="min sqft" value={this.props.globalState.min_square_feet} onChange={this.props.change}/>
-            <input type="text" name="max_square_feet" className="max-square-feet" placeholder="max sqft" value={this.props.globalState.max_square_feet} onChange={this.props.change}/>
-        </div>
-        <div id="bottom-filters" className="filter-extras">
-          <span className="title">
-            Extras
-          </span>
-          <label htmlFor="extras">
-            <span>Pool</span>
-            <input name="pool" value="pool" type="checkbox" onChange={this.props.change}/>
-          </label>
-          <label htmlFor="extras">
-            <span>Finished Basement</span>
-            <input name="finished_basement" value="finished_basement" type="checkbox" onChange={this.props.change}/>
-          </label>
-          <label htmlFor="extras">
-            <span>Two Car Garage</span>
-            <input name="two_car_garage" value="two_car_garage" type="checkbox" onChange={this.props.change}/>
-          </label>
-        </div>
-      </div>
+    <button onClick={this.showFilter}> Filter </button>
+      {
+        this.state.showFilter //new
+          ? ( //new
+              <div className="inside" ref={(element) => {this.dropMenu = element;}}>
+                <span className='title'>City</span>
+                <select name="neighborhood" className="filter-neighborhood" onChange={this.props.change}>
+                  <option value='All'>All</option>
+                    {this.neighborhoods()}
+                </select>
+                <span className='title'>Type</span>
+                <select name="houseType" className="filter-houseType" onChange={this.props.change}>
+                  <option value='All'>All</option>
+                  {this.houseTypes()}
+                </select>
+                <span className='title'>Bedrooms</span>
+                <select name="bedrooms" className=" filter-rooms" onChange={this.props.change}>
+                  {this.beds()}
+                </select>
+                <span className='title'>Baths</span>
+                <select name="baths" className="filter-baths" onChange={this.props.change}>
+                  {this.bath()}
+                </select>
+                <div className="filter-price">
+                  <span className="title">Price</span>
+                    <input type="text" name="min_price" className="min-price" placeholder="min price" value={this.props.globalState.min_price} onChange={this.props.change}/>
+                    <input type="text" name="max_price" className="max-price" placeholder="max price" value={this.props.globalState.max_price} onChange={this.props.change}/>
+                </div>
+                <div className="filter-square-feet">
+                  <span className="title">Square Feet</span>
+                    <input type="text" name="min_square_feet" className="min-square-feet" placeholder="min sqft" value={this.props.globalState.min_square_feet} onChange={this.props.change}/>
+                    <input type="text" name="max_square_feet" className="max-square-feet" placeholder="max sqft" value={this.props.globalState.max_square_feet} onChange={this.props.change}/>
+                </div>
+                <div id="bottom-filters" className="filter-extras">
+                  <span className="title">
+                    Extras
+                  </span>
+                  <label htmlFor="extras">
+                    <span>Pool</span>
+                    <input name="pool" value="pool" type="checkbox" onChange={this.props.change}/>
+                  </label>
+                  <label htmlFor="extras">
+                    <span>Finished Basement</span>
+                    <input name="finished_basement" value="finished_basement" type="checkbox" onChange={this.props.change}/>
+                  </label>
+                  <label htmlFor="extras">
+                    <span>Two Car Garage</span>
+                    <input name="two_car_garage" value="two_car_garage" type="checkbox" onChange={this.props.change}/>
+                  </label>
+                </div>
+              </div>
+            )
+            : (
+              null
+            )
+      }
     </section>
   );
 }
