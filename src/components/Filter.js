82127284d9f5439f0772ced5df5aa5fef725aa2a
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-09-20 22:28:13 -0500

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -69,9 +69,6 @@ render () {
   return (
     <section id="filter">
       <div className="inside">
-
-        <div className="filter-options-1">
-          <div className="filter-city-type">
             <span className='title'>City</span>
               <select name="neighborhood" className="filter-neighborhood" onChange={this.props.change}>
                 <option value='All'>All</option>
@@ -82,8 +79,6 @@ render () {
                 <option value='All'>All</option>
                   {this.houseTypes()}
               </select>
-          </div>
-          <div className="filter-bed-bath">
             <span className='title'>Bedrooms</span>
               <select name="bedrooms" className=" filter-rooms" onChange={this.props.change}>
                 {this.beds()}
@@ -92,27 +87,22 @@ render () {
               <select name="baths" className="filter-baths" onChange={this.props.change}>
                 {this.bath()}
               </select>
-          </div>
-        </div>
 
-        <div className="price-ft-filters">
-          <div className="filter-price">
             <span className="title">Price</span>
               <input type="text" name="min_price" className="min-price" placeholder="min price" value={this.props.globalState.min_price} onChange={this.props.change}/>
               <input type="text" name="max_price" className="max-price" placeholder="max price" value={this.props.globalState.max_price} onChange={this.props.change}/>
-          </div>
-          <div className="filter-square-feet">
+
             <span className="title">Square Feet</span>
               <input type="text" name="min_square_feet" className="min-square-feet" placeholder="min sqft" value={this.props.globalState.min_square_feet} onChange={this.props.change}/>
               <input type="text" name="max_square_feet" className="max-square-feet" placeholder="max sqft" value={this.props.globalState.max_square_feet} onChange={this.props.change}/>
-          </div>
-        </div>
+
 
         <div id="bottom-filters" className="filter-extras">
+          
           <span className="title">
             Extras
           </span>
-          <div className="extras-wrapper">
+          <div className='extras-wrapper'>
           <label htmlFor="extras">
             <span>Pool</span>
               <input name="pool" value="pool" type="checkbox" onChange={this.props.change}/>
@@ -125,7 +115,7 @@ render () {
             <span>Two Car Garage</span>
               <input name="two_car_garage" value="two_car_garage" type="checkbox" onChange={this.props.change}/>
           </label>
-        </div>
+          </div>
         </div>
       </div>
     </section>
