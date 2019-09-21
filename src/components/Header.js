5260ac520a8fff27a891b9f308f82b98066443a0
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-09-20 21:37:35 -0500

    start on dropdown filter

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,5 +23,35 @@ render () {
 }
 }
 
+export default Header;// import React from 'react';
+import React, {Component} from 'react';
+// import ReactDOM from 'react-dom';
+
+class Header extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Adam',
+    }
+  }
+// function Header() {
+render () {
+  return (
+      <header>
+        <div className='button'>
+          <i className="fa fa-bars"></i>
+        </div>
+        <div className="logo"><span className="home">HOME</span>ble</div>
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Login</a>
+          <a href="#" className="signup-button">Sign Up</a>
+        </nav>
+      </header>
+  );
+}
+}
+
 export default Header;
+
 
