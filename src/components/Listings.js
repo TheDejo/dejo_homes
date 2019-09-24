e95c3347964d53d82e4dca19d471f8e98a6c90f8
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-09-24 18:49:51 -0500

    Update Listings.js

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -8,17 +8,72 @@ class Listings extends Component {
       name: 'Adam',
     }
     this.loopListings = this.loopListings.bind(this)
+    this.neighborhoods = this.neighborhoods.bind(this)
+    this.houseTypes = this.houseTypes.bind(this)
+    this.beds = this.beds.bind(this)
+    this.bath = this.bath.bind(this)
+}
+
+componentWillMount() {
+  this.props.populateAction()
+}
+neighborhoods() {
+  if(this.props.globalState.populateFormsData.neighborhoods !== undefined) {
+    var {neighborhoods} = this.props.globalState.populateFormsData
+    console.log(neighborhoods);
+    return neighborhoods.map((item) => {
+      return (
+        <option key={item} value={item}>{item}</option>
+      )
+    })
   }
+}
+
+houseTypes() {
+  if(this.props.globalState.populateFormsData.houseTypes !== undefined) {
+    var {houseTypes} = this.props.globalState.populateFormsData
+    console.log(houseTypes);
+    return houseTypes.map((item) => {
+      return (
+        <option key={item} value={item}>{item}</option>
+      )
+    })
+  }
+}
+
+beds() {
+  if(this.props.globalState.populateFormsData.beds !== undefined) {
+    var {beds} = this.props.globalState.populateFormsData
+    console.log(beds);
+    return beds.map((item) => {
+      return (
+        <option key={item} value={item}>{item}+ br</option>
+      )
+    })
+  }
+}
+
+bath() {
+  if(this.props.globalState.populateFormsData.bath !== undefined) {
+    var {bath} = this.props.globalState.populateFormsData
+    console.log(bath);
+    return bath.map((item) => {
+      return (
+        <option key={item} value={item}>{item}+ ba</option>
+      )
+    })
+  }
+}
 loopListings () {
   const {listingsData} = this.props
-
   if(listingsData === undefined || listingsData.length === 0) {
     return "No results found"
   }
 
   return listingsData.map ((listing, index) => {
     if (this.props.globalState.view === 'grid') { // grid view
-      return (<div className="col-md-3" key={index}>
+      return (
+      <div className="col-md-3" key={index}>
         <div className="list">
           <div className="list-img" style={{background: `url("${listing.image}") no-repeat center center`}}>
             <span className="address">{listing.address}</span>
@@ -54,8 +109,9 @@ loopListings () {
           </div>
         </div>
       </div>)
-    } else { // horizontal view
-      return (<div className="col-md-12 col-lg-6" key={index}>
+      } else { // horizontal view
+      return (
+      <div className="col-md-12 col-lg-6" key={index}>
         <div className="list">
           <div className="list-img" style={{background: `url("${listing.image}") no-repeat center center`}}>
             <span className="address">{listing.address}</span>
@@ -92,21 +148,70 @@ loopListings () {
         </div>
       </div>)
     }
-
   })
 }
 // function Listings() {
 render () {
     return (
-      <section id="listings">
-        <section className="search-area">
-          <input type='text' name="search" placeholder="Search" onChange={this.props.change}/>
-        </section>
+    <div>
+      <section id="filter">
+        <div className="inside">
+          <span className='title'>City</span>
+            <select name="neighborhood" className="filter-neighborhood" onChange={this.props.change}>
+              <option value='All'>All</option>
+              {this.neighborhoods()}
+            </select>
+          <span className='title'>Type</span>
+            <select name="houseType" className="filter-houseType" onChange={this.props.change}>
+              <option value='All'>All</option>
+                {this.houseTypes()}
+            </select>
+          <span className='title'>Bedrooms</span>
+            <select name="bedrooms" className=" filter-rooms" onChange={this.props.change}>
+              {this.beds()}
+            </select>
+          <span className='title'>Baths</span>
+            <select name="baths" className="filter-baths" onChange={this.props.change}>
+              {this.bath()}
+            </select>
+          <span className="title">Price</span>
+            <input type="text" name="min_price" className="min-price" placeholder="min price" value={this.props.globalState.min_price} onChange={this.props.change}/>
+            <input type="text" name="max_price" className="max-price" placeholder="max price" value={this.props.globalState.max_price} onChange={this.props.change}/>
+          <span className="title">Square Feet</span>
+            <input type="text" name="min_square_feet" className="min-square-feet" placeholder="min sqft" value={this.props.globalState.min_square_feet} onChange={this.props.change}/>
+            <input type="text" name="max_square_feet" className="max-square-feet" placeholder="max sqft" value={this.props.globalState.max_square_feet} onChange={this.props.change}/>
+          <div id="bottom-filters" className="filter-extras">
+            <span className="title">
+              Extras
+            </span>
+            <div className='extras-wrapper'>
+              <label htmlFor="extras">
+                <span>Pool</span>
+                  <input name="pool" value="pool" type="checkbox" onChange={this.props.change}/>
+              </label>
+              <label htmlFor="extras">
+                <span>Finished Basement</span>
+                  <input name="finished_basement" value="finished_basement" type="checkbox" onChange={this.props.change}/>
+              </label>
+              <label htmlFor="extras">
+                <span>Two Car Garage</span>
+                  <input name="two_car_garage" value="two_car_garage" type="checkbox" onChange={this.props.change}/>
+              </label>
+            </div>
+          </div>
+        </div>
+      </section>
 
+
+      <section id="listings">
+        <div className="listing-wrapper">
         <section className="sort">
           <div className='main-results'>
             {this.props.globalState.filteredData.length} results found
           </div>
+          <section className="search-area">
+            <input type='text' name="search" placeholder="Search" onChange={this.props.change}/>
+          </section>
           <div className="sort-options">
             <select name="sort_by" className="sort_by" onChange={this.props.change}>
               <option value="price-descending">Lowest Price</option>
@@ -118,27 +223,24 @@ render () {
             </div>
           </div>
         </section>
-
         <section className="list-results">
-
-        <div className='bottom-row'>
-          {this.loopListings()}
-        </div>
+          <div className='bottom-row'>
+            {this.loopListings()}
+          </div>
         </section>
-
-
         <section id="pagination">
-            <ul className="pages">
-              <li>Prev</li>
-              <li className="active">1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
-              <li>Next</li>
-            </ul>
+          <ul className="pages">
+            <li>Prev</li>
+            <li className="active">1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>Next</li>
+          </ul>
         </section>
-
+        </div>
       </section>
+    </div>
   );
 }
 }
